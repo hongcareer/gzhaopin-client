43b44e2441ebf4dd3e88053432eeb5749227dfeb
hongcareer <hongcareer@126.com>
2018-12-05 14:10:44 +0800

    登陆注册生成cookie，没有cookie时，返回登录界面

--- src/components/main/index.jsx
+++ src/components/main/index.jsx
@@ -2,8 +2,15 @@ import React,{Component} from 'react';
 import {Route} from 'react-router-dom';
 import Bossinfo from '../../containers/boss-info';
 import Consumerinfo from '../../containers/consumer-info';
+import Cookies from 'js-cookie';
+import {Redirect} from 'react-router-dom'
 class Main extends Component{
+
   render(){
+    const userid = Cookies.get('userid');
+    if(!userid){
+      return <Redirect to='/login'/>
+    }
     return(
       <div>
         <Route path='/bossinfo' component={Bossinfo}/>
